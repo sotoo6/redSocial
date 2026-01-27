2ad7ac2dc0de7b3f98440765c14daf6352b4a7cf
Lucia <lu.soto.sanchez@gmail.com>
2026-01-27 09:10:24 +0100

    Corrección de rutas

--- app/Http/Middleware/AuthSession.php
+++ app/Http/Middleware/AuthSession.php
@@ -10,8 +10,14 @@ class Authsession
     public function handle(Request $request, Closure $next)
     {
         if (!session()->has('user')) {
-            // En vez de redirect('/login'):
-            return response()->view('auth.must_login', [], 403);
+
+            // Si intenta entrar al home "/", lo mandamos al login
+            if ($request->is('/')) {
+                return redirect('/login');
+            }
+
+            // Para cualquier otra ruta protegida, mostramos la vista "must login"
+            return response()->view('auth.must_login', [], 401);
         }
 
         return $next($request);
